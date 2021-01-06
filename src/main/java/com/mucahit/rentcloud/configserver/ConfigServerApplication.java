daa4a23f0be8446eac4f55767a73c610ad5f82b2
mucahitsahin <mucahit.sahin@anadoluefes.com>
2021-01-06 13:45:16 +0300

    package name updated

--- src/main/java/com/mucahit/rentcloud/configserver/ConfigServerApplication.java
+++ src/main/java/com/mucahit/rentcloud/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.krishantha.rentcloud.configserver;
+package com.mucahit.rentcloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
